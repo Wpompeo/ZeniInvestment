cd35246ac664c469b0c78f32b380fac3e631e40b
Wagner Pompeo <wag_pompeo@yahoo.com.br>
2026-08-07 16:54:24 -0300

    three commit - Development new files and functions

--- app/src/main/java/com/wts/zeniinvestment/MainActivity.kt
+++ app/src/main/java/com/wts/zeniinvestment/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.wts.zeniinvestment
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,11 @@ import com.wts.zeniinvestment.adapter.TipsAdapter
 import com.wts.zeniinvestment.model.Tip
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dotsLayout: LinearLayout
+    private lateinit var dots: Array<ImageView>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        //lista de mensagens da tela inicial
         val tips = listOf(
 
             Tip(
@@ -38,12 +46,80 @@ class MainActivity : AppCompatActivity() {
                 R.drawable.ic_bank,
                 "Reserva de Emergência",
                 "Tenha entre 6 e 12 meses de despesas antes de investir em renda variável."
+            ),
+
+            Tip(
+                R.drawable.ic_start,
+                "Simulação inteligente",
+                "Calcule projeções precisas com juros compostos."
+            ),
+
+            Tip(
+                R.drawable.ic_invest,
+                "Pronto para começar?",
+                "Configure sua simulação e descubra o potencial dos seus investimentos."
             )
 
+
+
         )
+
+        fun createDots(size: Int, currentPosition: Int) {
+
+            dots = Array(size) { ImageView(this) }
+
+            dotsLayout.removeAllViews()
+
+            for (i in 0 until size) {
+
+                dots[i] = ImageView(this)
+
+                if (i == currentPosition)
+                    dots[i].setImageResource(R.drawable.dot_selected)
+                else
+                    dots[i].setImageResource(R.drawable.dot_unselected)
+
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+
+                params.setMargins(8, 0, 8, 0)
+
+                dotsLayout.addView(dots[i], params)
+            }
+        }
 
         val viewPager = findViewById<ViewPager2>(R.id.viewPagerInit)
 
         viewPager.adapter = TipsAdapter(tips)
+        viewPager.registerOnPageChangeCallback(
+
+            object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+
+                    createDots(tips.size, position)
+                }
+
+            }
+
+        )
+        dotsLayout = findViewById(R.id.dotsLayout)
+
+        createDots(tips.size, 0)
+        viewPager.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+
+                    // muda a bolinha selecionada
+                }
+            }
+        )
+
+
     }
 }
